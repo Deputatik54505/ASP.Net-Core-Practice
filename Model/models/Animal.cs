fb09ab73730347e4d7b1d48f1cf07aa4d95df8c9
Ivan Platonov <platonov.vania0-0@yandex.ru>
2023-08-05 05:52:01 +0300

    Functional from EntityPracticeModel migrated to ASPPractice.model

---
+++ Model/models/Animal.cs
@@ -0,0 +1,20 @@
+﻿namespace Model.models;
+/// <summary>
+/// Entity which represent animal
+/// </summary>
+public class Animal
+{
+	public Animal(string animalName)
+    {
+        this.AnimalName = animalName;
+    }
+	public int Id { get; set; }
+	public string AnimalName { get; set; }
+	public Cell Cell { get; set; }
+	public int CellId { get; set; }
+	public Meal MainMeal { get; set; }
+	public int MainMealId { get; set; }
+
+	public List<Zookeeper> Zookeepers { get; set; } = new();
+
+}
